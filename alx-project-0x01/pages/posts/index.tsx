0522cb88e803f4b6d8216192a025fdcaeed674a7
edwincodee <edwiinnemmanuell@gmail.com>
2025-07-16 20:49:29 +0100

    Set up navigation between pages

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,6 +1,15 @@
+import Header from "@/components/layout/Header"
+
 const Posts: React.FC = () => {
     return (
-        <div>Posts</div>
+        <div>
+            <Header />
+            <main>
+                <h3>
+                    Post
+                </h3>
+            </main>
+        </div>
     )
 }
 
--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,6 +1,15 @@
+import Header from "@/components/layout/Header"
+
 const Users: React.FC = () => {
     return (
-        <div>Users</div>
+        <div>
+            <Header/>
+            <main>
+                <h3>
+                    posts
+                </h3>
+            </main>
+        </div>
     )
 }
 
