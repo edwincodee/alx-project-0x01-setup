c5645a12b32beedde6c9756ac7dfcd3282d0add1
edwincodee <edwiinnemmanuell@gmail.com>
2025-07-16 20:38:37 +0100

    more project setup

---
+++ alx-project-0x01/components/common/Button.tsx
@@ -0,0 +1,7 @@
+const Button: React.FC = () => {
+    return (
+        <div> Button</div>
+    )
+}
+
+export default Button
---
+++ alx-project-0x01/components/common/PostCard.tsx
@@ -0,0 +1,9 @@
+const PostCard: React.FC = () => {
+    return (
+        <div>
+            Post Card
+        </div>
+    )
+}
+
+export default PostCard
