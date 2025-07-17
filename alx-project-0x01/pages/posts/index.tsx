f51b91029cbd939f7ef0b466cdfab55624027ca2
edwincodee <edwiinnemmanuell@gmail.com>
2025-07-17 01:43:30 +0100

    More Components (Extending Post Page)

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,20 +1,28 @@
 import PostCard from "@/components/common/PostCard"
+import PostModal from "@/components/common/PostModal"
 import Header from "@/components/layout/Header"
-import { PostProps } from "@/interfaces"
+import { PostProps, PostData } from "@/interfaces"
+import { useState } from "react"
 
 interface IPost {
     posts: PostProps[]
 }
 
 const Posts: React.FC<IPost> = ({posts}) => {
-    console.log(posts)
+    const [isModalOpen, setModalOpen] = useState<boolean>(false)
+    const [post, setPost] = useState<PostData | null>(null)
+
+    const handleAddPost = (newPost: PostData) => {
+        setPost({...newPost, id: posts.length + 1})
+    }
+    // console.log(posts)
     return (
         <div className="flex flex-col h-screen">
             <Header />
             <main className="p-4">
                 <div className="flex justify-between">
                     <h1 className="text-2xl font-semibold">Post Content</h1>
-                    <button className="bg-blue-700 px-4 py-2 rounded-full text-white">
+                    <button onClick={()=> setModalOpen(true)} className="bg-blue-700 px-4 py-2 rounded-full text-white">
                         Add Post
                     </button>
                 </div>
@@ -24,6 +32,8 @@ const Posts: React.FC<IPost> = ({posts}) => {
                     ))}
                 </div>
             </main>
+
+            {isModalOpen && (<PostModal onClose={() => setModalOpen(false)} onSubmit={handleAddPost} />)}
         </div>
     )
 }
